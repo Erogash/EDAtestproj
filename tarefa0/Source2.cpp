34ac1b8372215b89ae99bac1a509df6f55a8f78c
Unknown <correiododanilo@hotmail.com>
2018-04-11 21:56:12 +0100

    test2

---
+++ tarefa0/Source2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string nome;
+	cout << "Introduza o Seu Primeio e ultimo Nome \n";
+	getline(cin, nome);
+	cout << "Ola " << nome << "\n";
+	return 0;
+}aaaaaaaaaaaaaaaaaaaaaaa
